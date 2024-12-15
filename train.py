b579494f27bc95d00cfbb67c17792ffa9f1d9de6
xNTsai <johnnytsai920315@gmail.com>
2024-12-15 18:21:11 +0800

    modify transforms

--- train.py
+++ train.py
@@ -176,9 +176,8 @@ class LocalDataProcessor:
         ]
 
         transform = transforms.Compose([
-            # transforms.Resize((224, 224)),
+            transforms.Resize((720, 1280)),  # Resize all images to a fixed size
             transforms.ToTensor(),
-            # transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
         ])
 
         # Load training datasets
